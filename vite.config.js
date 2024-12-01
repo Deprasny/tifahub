c1f6e8104eb94d98a12d2cb268dd500111aa5531
deprasny <deprasny@gmail.com>
2024-12-02 00:43:18 +0700

    Fix: Update build configuration for proper asset loading

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tifahub/',
+  base: './',
   build: {
-    outDir: 'build'
+    outDir: 'build',
+    assetsDir: 'assets',
+    emptyOutDir: true,
   }
 })
